2713cc61c81f874cccdb2efed98935de125f1bc9
Ikanor1221 <ilya.bochkov97@gmail.com>
2023-07-25 20:21:19 +0300

    Implement basic GIF update

---
+++ script.js
@@ -0,0 +1,8 @@
+const img = document.querySelector('img');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=RlxzmTHnX1GG2aus3Yx9bWB6fJ6hmGqE&s=cats', {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    img.src = response.data.images.original.url
+  });
