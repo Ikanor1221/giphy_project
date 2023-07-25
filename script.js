047205607706128d50e922c735040a96d54ec569
Ikanor1221 <ilya.bochkov97@gmail.com>
2023-07-26 00:30:46 +0300

    Implement text search functionality

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+"use strict";
+
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=RlxzmTHnX1GG2aus3Yx9bWB6fJ6hmGqE&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+
+const button = document.querySelector("#refresh_button");
+
+const input = document.querySelector("#search_field");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  updatePicture(input.value);
+  return;
+});
+
+function updatePicture(search) {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=RlxzmTHnX1GG2aus3Yx9bWB6fJ6hmGqE&s=" +
+      search,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch(function (error) {
+      console.log("No results!");
+      input.value = "No results!";
+      img.src =
+        "https://media0.giphy.com/media/zLCiUWVfex7ji/giphy.gif?cid=c9074619olsqnjmnmru4jlukyt6jd4uijd4bq119jdopqbcb&ep=v1_gifs_translate&rid=giphy.gif&ct=g";
+    });
+
+  return;
+}
